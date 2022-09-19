9e1f84c1b9a665d11133f5dfe75c0532cb1e94ea
Shota-Yoshida-byte <83653336+Shota-Yoshida-byte@users.noreply.github.com>
2022-09-19 12:22:50 +0000

    919

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,23 @@
 class UsersController < ApplicationController
   def show
-    @user =　#XXX
-    @post_images = @user.post_images      end
+    @user = User.find(params[:id])
+    @post_images = @user.post_images
+  end
 
   def edit
-    @user = XXX
+    @user = #XXX
   end
+
+  def update
+    @user = ユーザーの取得
+    @user.ユーザーのアップデート
+    redirect_to ユーザーの詳細ページへのパス
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'bookes/new'
+  get 'bookes/show'
+  get 'bookes/edit'
   devise_for :users
   root to: 'homes#top'
   get "homes/about" => "homes#about", as: "about"
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 end
